b25124302eedce6ba0feadfbe34157645ff389d9
danielcorralesleon3 <dcorrales@nigul.cide.es>
2024-02-11 19:56:33 +0100

    Actividad 3.11

---
+++ Actividades/my-react-app/src/componentes/TodoList.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+function ListaDeTareas() {
+  
+  const [listatareas, setTareas] = useState([]);
+  const [nuevaTarea, setNuevaTarea] = useState('');
+
+  const agregarTarea = () => {
+      setTareas([...listatareas, nuevaTarea]);
+  };
+
+  const eliminarTarea = (index) => {
+    const nuevasTareas = [...listatareas];
+    nuevasTareas.splice(index, 1);
+    setTareas(nuevasTareas);
+  };
+
+  return (
+    <div>
+      <h1>Lista de Tareas</h1>
+      <input type="text" value={nuevaTarea} onChange={(e) => setNuevaTarea(e.target.value)} placeholder="Añade una tarea a tu gusto"/>
+      <button onClick={agregarTarea}>Agregar Tarea</button>
+      <ul>
+        {listatareas.map((tarea, index) => (
+          <li key={index}>
+            {tarea}
+            <button onClick={() => eliminarTarea(index)}>Eliminar</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ListaDeTareas;
