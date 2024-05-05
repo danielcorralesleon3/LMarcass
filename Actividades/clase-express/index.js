86ee912ed5dd1c7f5cdbe670bbf2a5dacce7e612
danielcorralesleon3 <dcorrales@nigul.cide.es>
2024-05-05 20:15:30 +0200

    actividad 6.1

--- Actividades/clase-express/index.js
+++ Actividades/clase-express/index.js
@@ -9,26 +9,52 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
 
+//ejercicio 6.1
+
+  app.get('/usuaris', (req, res) => {
+    resultadoSelect=req.query.id;
+    const rows=db.prepare("SELECT * from usuarios").all();
+    res.json(rows)
+  })
+
+  app.get('/usuari', (req, res) => {
+    usuariosID=req.query.id;
+    const rows=db.prepare("SELECT * from usuarios where ID=?").get(usuariosID);
+    res.json(rows)
+  })
+
+  app.get('/productes', (req, res) => {
+    resultadoSelect=req.query.id;
+    const rows=db.prepare("SELECT * from productes").all();
+    res.json(rows)
+  })
+
+
+  app.get('/producte', (req, res) => {
+    productesID=req.query.id;
+    const rows=db.prepare("SELECT * from productes where ID=?").get(productesID);
+    res.json(rows)
+  })
+
+  app.get('/comandes', (req, res) => {
+    resultadoSelect=req.query.id;
+    const rows=db.prepare("SELECT * from comandes c join productes p on c.producte_id=p.id join usuarios u on c.usuario_id=u.id ").all();
+    res.json(rows)
+  })
+
+  app.get('/comanda', (req, res) => {
+    productesID=req.query.id;
+    const rows=db.prepare("SELECT * from comandes c join productes p on c.producte_id=p.id join usuarios u on c.usuario_id=u.id and C.ID=?").get(productesID);
+    res.json(rows)
+  })
+
+///////////////////////////////////////////////////////////////////////////////
+
+
   app.get("/",(req,res)=>{
     resultadoSelect=req.query.id;
     const rows=db.prepare("SELECT * from usuarios").all();
     res.render("index",msgs=rows);
-  })
-
-
-
-  app.get('/usuarios', (req, res) => {
-    //select
-    resultadoSelect=req.query.id;
-    const rows=db.prepare("SELECT * from usuarios").all();
-    res.json(rows)
-  })
-
-  app.get('/usuario', (req, res) => {
-    //select
-    usuariosID=req.query.id;
-    const rows=db.prepare("SELECT * from usuarios where ID=?").get(usuariosID);
-    res.json(rows)
   })
 
   app.get('/persona', (req, res) => {
