c5505ea7c8d48c4c0c8c38740d927c6879e5d34d
danielcorralesleon3 <dcorrales@nigul.cide.es>
2024-05-08 15:44:07 +0200

    actividad 6.2

--- Actividades/clase-express/index.js
+++ Actividades/clase-express/index.js
@@ -49,7 +49,42 @@ app.use(express.urlencoded({extended:true}))
   })
 
 ///////////////////////////////////////////////////////////////////////////////
+// Actividad 6.2
 
+app.get('/usuario', (req, res) => {
+  res.render("usuario")
+})
+
+app.post("/usuario",(req,res) =>{
+   console.log(req.body)
+   if (!req.body.nombre || !req.body.email) {
+    throw new Error ("Nombre o email incorrecto");
+  }
+   if (req.body.nombre && req.body.email) {
+     const statement=db.prepare("insert into usuarios (nombre,email)VALUES (?,?)")
+     statement.run(req.body.nombre,req.body.email)
+   }
+   res.redirect("usuario")
+})
+
+app.get('/producto', (req, res) => {
+  res.render("producto")
+})
+
+app.post("/producto",(req,res) =>{
+  console.log(req.body)
+  if (!req.body.nombre || !req.body.precio ||req.body.precio<0) {
+    throw new Error ("Nombre o precio incorrecto");
+  }
+  if (req.body.nombre && req.body.precio) {
+    const statement=db.prepare("insert into productes (nombre,precio)VALUES (?,?)")
+    statement.run(req.body.nombre,req.body.precio)
+  }
+  res.redirect("producto")
+})
+
+
+///////////////////////////////////////////////////////////////////////////////////
 
   app.get("/",(req,res)=>{
     resultadoSelect=req.query.id;
@@ -57,30 +92,7 @@ app.use(express.urlencoded({extended:true}))
     res.render("index",msgs=rows);
   })
 
-  app.get('/persona', (req, res) => {
-    //select
-    //personaId=req.query.id;
-    //const rows=db.prepare("SELECT * from usuarios where id=?").get(personaId)
-    //res.json(rows)
-    //console.log(rows);
-    res.render("persona")
+
+  app.listen(port,()=>{
+    console.log(`Example app listening on port ${port}`)
   })
-
-
-
-app.post("/persona",(req,res) =>{
-   // personaId=req.body.id;
-   // const rows=db.prepare("SELECT * from personas where id=?").get(personaId)
-  //  res.json(rows)
-   // console.log(rows);
-    console.log(req.body)
-    if (req.body.nombre && req.body.apellidos) {
-      const statement=db.prepare("insert into personas (nombre,apellidos)VALUES (?,?)")
-      statement.run(req.body.nombre,req.body.apellidos)
-      console.log(info)
-    }
-    res.redirect("persona")
-})
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
