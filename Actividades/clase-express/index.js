69c755f69b8b9433efaf6c95e185e4c6a21f9f2c
danielcorralesleon3 <dcorrales@nigul.cide.es>
2024-05-31 20:29:45 +0200

    actividad 6.5

--- Actividades/clase-express/index.js
+++ Actividades/clase-express/index.js
@@ -4,47 +4,47 @@ const db = require("better-sqlite3")("usuarios.sqlite");
 const app = express()
 const port = 3000
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
 
 //ejercicio 6.1
 
-  app.get("/usuarios", (req, res) => {
-    resultadoSelect=req.query.id;
-    const rows=db.prepare("SELECT * from usuarios").all();
-    console.log(rows);
-    res.render("usuarios", usuarios = rows)
-  })
-
-  app.get("/usuari", (req, res) => {
-    usuariosID=req.query.id;
-    const rows=db.prepare("SELECT * from usuarios where ID=?").get(usuariosID);
-    res.json(rows)
-  })
-
-  app.get("/productos", (req, res) => {
-    resultadoSelect=req.query.id;
-    const rows=db.prepare("SELECT * from productes").all();
-    res.render("productos" ,{producto: rows})
-  })
-
-
-  app.get("/producte", (req, res) => {
-    productesID=req.query.id;
-    const rows=db.prepare("SELECT * from productes where ID=?").get(productesID);
-    res.json(rows)
-  })
-
-  app.get("/comandes", (req, res) => {
-    resultadoSelect=req.query.id;
-    const rows=db.prepare("SELECT * from comandes c join productes p on c.producte_id=p.id join usuarios u on c.usuario_id=u.id ").all();
-    
-    res.render("comandes", {comanda: rows})
-  })
-
+app.get("/usuarios", (req, res) => {
+  resultadoSelect = req.query.id;
+  const rows = db.prepare("SELECT * from usuarios").all();
  
+  res.render("usuarios", usuarios = rows)
+})
+
+app.get("/usuari", (req, res) => {
+  usuariosID = req.query.id;
+  const rows = db.prepare("SELECT * from usuarios where ID=?").get(usuariosID);
+  res.json(rows)
+})
+
+app.get("/productos", (req, res) => {
+  resultadoSelect = req.query.id;
+  const rows = db.prepare("SELECT * from productes").all();
+  res.render("productos", { producto: rows })
+})
+
+
+app.get("/producte", (req, res) => {
+  productesID = req.query.id;
+  const rows = db.prepare("SELECT * from productes where ID=?").get(productesID);
+  res.json(rows)
+})
+
+app.get("/comandes", (req, res) => {
+  resultadoSelect = req.query.id;
+  const rows = db.prepare("SELECT * from comandes c join productes p on c.producte_id=p.id join usuarios u on c.usuario_id=u.id ").all();
+
+  res.render("comandes", { comanda: rows })
+})
+
+
 
 ///////////////////////////////////////////////////////////////////////////////
 // Actividad 6.2
@@ -53,30 +53,30 @@ app.get("/usuario", (req, res) => {
   res.render("usuario")
 })
 
-app.post("/usuario",(req,res) =>{
-   console.log(req.body)
-   if (!req.body.nombre || !req.body.email) {
-    throw new Error ("Nombre o email incorrecto");
+app.post("/usuario", (req, res) => {
+  console.log(req.body)
+  if (!req.body.nombre || !req.body.email) {
+    throw new Error("Nombre o email incorrecto");
   }
-   if (req.body.nombre && req.body.email) {
-     const statement=db.prepare("insert into usuarios (nombre,email)VALUES (?,?)")
-     statement.run(req.body.nombre,req.body.email)
-   }
-   res.redirect("usuario")
+  if (req.body.nombre && req.body.email) {
+    const statement = db.prepare("insert into usuarios (nombre,email)VALUES (?,?)")
+    statement.run(req.body.nombre, req.body.email)
+  }
+  res.redirect("usuario")
 })
 
 app.get("/producto", (req, res) => {
   res.render("producto")
 })
 
-app.post("/producto",(req,res) =>{
+app.post("/producto", (req, res) => {
   console.log(req.body)
-  if (!req.body.nom || !req.body.precio ||req.body.precio<0) {
-    throw new Error ("Nombre o precio incorrecto");
+  if (!req.body.nom || !req.body.precio || req.body.precio < 0) {
+    throw new Error("Nombre o precio incorrecto");
   }
   if (req.body.nom && req.body.precio) {
-    const statement=db.prepare("insert into productes (nom,precio)VALUES (?,?)")
-    statement.run(req.body.nom,req.body.precio)
+    const statement = db.prepare("insert into productes (nom,precio)VALUES (?,?)")
+    statement.run(req.body.nom, req.body.precio)
   }
   res.redirect("producto")
 })
@@ -94,17 +94,17 @@ app.get("/detalles", (req, res) => {
 ///////////////////////////////////////////////////////////////////////////////////
 // Actividad 6.4
 
-app.get("/comanda", (req, res) =>{
+app.get("/comanda", (req, res) => {
   const rowsproductos = db.prepare("SELECT * from productes").all()
   const rowsusuario = db.prepare("SELECT * from usuarios").all()
 
-  res.render("comanda", {usuarios: rowsusuario, productes: rowsproductos});
+  res.render("comanda", { usuarios: rowsusuario, productes: rowsproductos });
 })
 app.post("/comanda", (req, res) => {
-    if (req.body.usuario_id && req.body.producte_id) {
-      const statement = db.prepare("INSERT INTO comandes (usuario_id, producte_id) VALUES (?,?)")
-      statement.run(req.body.usuario_id, req.body.producte_id)
-    }
+  if (req.body.usuario_id && req.body.producte_id) {
+    const statement = db.prepare("INSERT INTO comandes (usuario_id, producte_id) VALUES (?,?)")
+    statement.run(req.body.usuario_id, req.body.producte_id)
+  }
   res.redirect("comandes");
 })
 //////////////////////////////////////////////////////////////////////
@@ -113,26 +113,67 @@ app.post("/comanda", (req, res) => {
 
 app.get("/usuarioedit", (req, res) => {
   const ID = req.query.ID
-  const usuario = db.prepare("select * from usuarios where ID = ?").get(ID)
-  res.render("usuarioedit", {usuario: usuario});
+  const selectusuario = db.prepare("SELECT * from usuarios where ID = ?").get(ID);
+  res.render("usuarioedit", { selectusuario: selectusuario });
 })
 
 app.post("/usuarioedit", (req, res) => {
 
-    if (req.body.nombre && req.body.email && req.body.ID) {
-      const statement = db.prepare("UPDATE usuarios SET nombre = ?, email = ? WHERE ID = ?")
-      statement.run( req.body.nombre, req.body.email, req.body.ID)
-    }
-    res.redirect("usuarios");
+  console.log(req.body)
+  const statement = db.prepare("UPDATE usuarios SET nombre = ?, email = ? WHERE ID = ?")
+  statement.run(req.body.nombre, req.body.email, req.body.ID)
+
+
+  res.redirect("usuarios");
+})
+
+app.get("/producteedit", (req, res) => {
+  const id = req.query.id;
+  const producte_select = db.prepare("SELECT * from productes where id = ?").get(id)
+  res.render("producteedit", { producte_select: producte_select });
+})
+
+
+app.post("/producteedit", (req, res) => {
+  console.log(req.body)
+  if (req.body.nom && req.body.precio && req.body.id) {
+    const statement = db.prepare("UPDATE productes SET nom = ?, precio = ? WHERE id = ?")
+    statement.run(req.body.nom, req.body.precio, req.body.id)
+  }
+  res.redirect("productos");
 })
 
 
 
+app.get("/comandesedit", (req, res) => {
+  const id = req.query.id
+  const usuarios = db.prepare("SELECT * from usuarios").all()
+  const productes = db.prepare("SELECT * from productes").all()
+  const comanda = db.prepare("SELECT * from comandes where id = ?").get(id)
+  res.render("comandesedit", { comanda: comanda, usuarios: usuarios, productes: productes });
+
+})
+
+
+app.post("/comandesedit", (req, res) => {
+  const statement = db.prepare("UPDATE comandes SET usuario_id = ?, producte_id = ? WHERE id = ?")
+  statement.run(req.body.productes, req.body.usuarios, req.body.comanda)
+
+
+  if (req.body.usuarios && req.body.productes && req.body.id) {
+    const statement = db.prepare("UPDATE comandes SET usuario_id = ?, producte_id = ? WHERE id = ?")
+    statement.run(req.body.usuarios, req.body.productes, req.body.id)
+
+  }
+  res.redirect("comandes");
+  console.log(req.body)
+})
 
 
 ///////////////////////////////////////////////////////////////////////////////////
 
 
-  app.listen(port,()=>{
-    console.log(`Example app listening on port ${port}`)
-  })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
